b766d0f16a21eb4778e682623a1cd738e21f7418
FloowLite <96964508+FloowLite@users.noreply.github.com>
2022-05-12 20:49:17 +1200

    Initial commit

---
+++ Task1.cs
@@ -0,0 +1,47 @@
+﻿namespace Assignment2_MECHENG313 {
+    class FiniteStateTable
+    {
+        private struct cell_FST
+        {
+            public int nextState;
+            public Action[] actions;
+        }
+        private cell_FST[,] FST;
+        private static void do_nothing() { }
+
+        public FiniteStateTable(int num_states, int num_events)
+        {
+            this.FST = new cell_FST[num_states, num_events];
+            for (int i = 0; i < num_states; i++)
+            {
+                for (int j = 0; j < num_events; j++)
+                {
+                    this.FST[i, j].nextState = i;
+                    this.FST[i, j].actions = new Action[] { do_nothing };
+                }
+            }
+
+        }
+
+        public void SetNextState(int state, int event_num, int next_state_num)
+        {
+            this.FST[state, event_num].nextState = next_state_num;
+        }
+
+        public void SetActions(int state_num, int event_num, Action[] actions)
+        {
+            this.FST[state_num, event_num].actions = actions;
+        }
+
+        public int GetNextState(int state, int event_num)
+        {
+            return this.FST[state, event_num].nextState;
+        }
+
+        public Action[] GetActions(int state, int event_num)
+        {
+            return this.FST[state, event_num].actions;
+        }
+    }
+
+}
---
+++ Task2.cs
@@ -0,0 +1,81 @@
+﻿namespace Assignment2_MECHENG313
+{
+    class Task2
+    {
+
+        public static void actionW()
+        {
+            Console.WriteLine("Action W");
+        }
+
+        public static void actionX()
+        {
+            Console.WriteLine("Action X");
+        }
+
+        public static void actionY()
+        {
+            Console.WriteLine("Action Y");
+        }
+
+        public static void actionZ()
+        {
+            Console.WriteLine("Action Z");
+        }
+
+        public static void quit()
+        {   
+            Console.WriteLine("Please enter the log save file directory and name (e.g. c:\\temp\\log1.txt");
+            string dir_input = Console.ReadLine();
+            Console.WriteLine("Output has been successfully logged to ...");
+            Environment.Exit(0);
+        }
+
+        public static void Main(string[] args)
+        {
+            Dictionary<char, int> event_to_num = new Dictionary<char, int>();
+            event_to_num['a'] = 0;
+            event_to_num['b'] = 1;
+            event_to_num['c'] = 2;
+            event_to_num['q'] = 3;
+
+            var fst = new FiniteStateTable(3, 4);
+            fst.SetNextState(0, 0, 1);
+            fst.SetActions(0, 0, new Action[] { actionX, actionY });
+
+            fst.SetNextState(1, 0, 0);
+            fst.SetActions(1, 0, new Action[] { actionW });
+
+            fst.SetNextState(1, 1, 2);
+            fst.SetActions(1, 1, new Action[] { actionX, actionZ });
+
+            fst.SetNextState(2, 2, 1);
+            fst.SetActions(2, 2, new Action[] { actionX, actionY });
+
+            fst.SetNextState(2, 0, 0);
+            fst.SetActions(2, 0, new Action[] { actionW });
+
+            fst.SetActions(0, 3, new Action[] { quit });
+            fst.SetActions(1, 3, new Action[] { quit });
+            fst.SetActions(2, 3, new Action[] { quit });
+
+            int state = 0;
+            Console.WriteLine("State: {0}", state);
+            while (true)
+            {
+                string event_input = Console.ReadLine();
+                if (event_input.Length == 1 && event_to_num.ContainsKey(event_input[0]))
+                {
+                    int event_num = event_to_num[event_input[0]];
+                    Action[] actions = fst.GetActions(state, event_num);
+                    for (int i = 0; i < actions.Length; i++){actions[i]();}
+                    if (state != fst.GetNextState(state, event_num))
+                    {
+                        state = fst.GetNextState(state, event_num);
+                        Console.WriteLine("State: {0}", state);
+                    }
+                }
+            }
+        }
+    }
+}
