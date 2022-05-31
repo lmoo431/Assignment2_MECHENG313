a68621432679807f51b817171ac149ab941e5b0d
jkir374 <87788428+jkir374@users.noreply.github.com>
2022-05-31 23:46:23 +1200

    Update Task1 to fit with changes made for Task3

--- Task1.cs
+++ Task1.cs
@@ -5,11 +5,14 @@
         {
             public int nextState;
             public Action[] actions;
+            public bool isDependent;
+            public int dependent_state;
+            public FiniteStateTable dependent_FST;
         }
         private cell_FST[,] FST;
         private static void do_nothing() { }
 
-        public FiniteStateTable(int num_states, int num_events)
+        public FiniteStateTable(int num_states, int num_events, int init_state)
         {
             this.FST = new cell_FST[num_states, num_events];
             for (int i = 0; i < num_states; i++)
@@ -18,9 +21,12 @@
                 {
                     this.FST[i, j].nextState = i;
                     this.FST[i, j].actions = new Action[] { do_nothing };
+                    this.FST[i, j].isDependent = false;
+                    this.FST[i, j].dependent_state = 0;
+                    this.FST[i, j].dependent_FST = null;
                 }
             }
-
+            this.currentState = init_state;
         }
 
         public void SetNextState(int state, int event_num, int next_state_num)
@@ -32,15 +38,45 @@
         {
             this.FST[state_num, event_num].actions = actions;
         }
+        
+        public void SetActions(int state_num, int event_num, Action[] actions, FiniteStateTable dependent_fst, int dependent_state_num)
+        {
+            this.FST[state_num, event_num].actions = actions;
+            this.FST[state_num, event_num].isDependent = true;
+            this.FST[state_num, event_num].dependent_state = dependent_state_num;
+            this.FST[state_num, event_num].dependent_FST = dependent_fst;
+        }
 
         public int GetNextState(int state, int event_num)
         {
+            if (this.FST[state, event_num].isDependent)
+            {
+                if (this.FST[state, event_num].dependent_FST.currentState == this.FST[state, event_num].dependent_state)
+                {
+                    return this.FST[state, event_num].nextState;
+                } else
+                {
+                    return state;
+                }
+            } else
+            {
             return this.FST[state, event_num].nextState;
+            }
         }
 
         public Action[] GetActions(int state, int event_num)
         {
+            if (this.FST[state, event_num].isDependent)
+            {
+                 if (this.FST[state, event_num].dependent_FST.currentState == this.FST[state, event_num].dependent_state)
+                     {
+                        return this.FST[state, event_num].actions;
+                     }
+                return new Action[] { do_nothing };
+            } 
+            else {
             return this.FST[state, event_num].actions;
+            }
         }
     }
 
